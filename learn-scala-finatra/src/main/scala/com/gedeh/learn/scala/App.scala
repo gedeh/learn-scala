25623259481abae691483684f7b3082a21c9cb11
gsaputra <gede.saputra@verint.com>
2019-08-03 18:37:33 +0100

    Move to sub directory

--- learn-scala-finatra/src/main/scala/com/gedeh/learn/scala/App.scala
+++ learn-scala-finatra/src/main/scala/com/gedeh/learn/scala/App.scala
@@ -1,7 +1,9 @@
+package com.gedeh.learn.scala
+
+import com.gedeh.learn.scala.controllers.HelloController
 import com.twitter.finatra.http.HttpServer
 import com.twitter.finatra.http.filters.CommonFilters
 import com.twitter.finatra.http.routing.HttpRouter
-import controllers.HelloController
 
 object AppMain extends AppServer
 
--- learn-scala-finatra/src/main/scala/com/gedeh/learn/scala/controllers/HelloController.scala
+++ learn-scala-finatra/src/main/scala/com/gedeh/learn/scala/controllers/HelloController.scala
@@ -1,18 +1,22 @@
-package controllers
+package com.gedeh.learn.scala.controllers
 
 import com.google.inject.Singleton
 import com.twitter.finagle.http.Request
 import com.twitter.finatra.http.Controller
+import com.twitter.inject.Logging
 
 @Singleton
-class HelloController extends Controller {
+class HelloController extends Controller with Logging {
+
   get("/hello") { request: Request =>
+    info("/hello endpoint")
     "Hello!"
   }
 
   get("/name/:name") { request: Request =>
     val name = request.params("name")
     val message = String.format("Hey %s!", name)
-    response.ok( message ).contentType("text/plain")
+    info(String.format("/name endpoint, name: %s", name))
+    response.ok(Map("message" -> message)).contentTypeJson()
   }
 }
