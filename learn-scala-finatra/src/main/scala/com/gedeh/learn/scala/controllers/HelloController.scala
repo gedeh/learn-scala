2e2a9379a0baa34d8d9f09e6449e3c352ae4f055
gsaputra <gede.saputra@verint.com>
2019-08-03 20:17:05 +0100

    try typed request and response

--- learn-scala-finatra/src/main/scala/com/gedeh/learn/scala/controllers/HelloController.scala
+++ learn-scala-finatra/src/main/scala/com/gedeh/learn/scala/controllers/HelloController.scala
@@ -3,20 +3,27 @@ package com.gedeh.learn.scala.controllers
 import com.google.inject.Singleton
 import com.twitter.finagle.http.Request
 import com.twitter.finatra.http.Controller
+import com.twitter.finatra.request.RouteParam
 import com.twitter.inject.Logging
+
+case class NameRequest(
+  @RouteParam name: String
+)
+
+case class NameResponse(
+  message: String
+)
 
 @Singleton
 class HelloController extends Controller with Logging {
-
   get("/hello") { request: Request =>
     debug("/hello endpoint")
     "Hello!"
   }
 
-  get("/name/:name") { request: Request =>
-    val name = request.params("name")
-    val message = String.format("Hey %s!", name)
-    debug(String.format("/name endpoint, name: %s", name))
-    response.ok(Map("message" -> message)).contentTypeJson()
+  get("/name/:name") { request: NameRequest =>
+    val message = String.format("Hey %s!", request.name)
+    debug(String.format("/name endpoint, name: %s", request.name))
+    response.ok(NameResponse(message)).contentTypeJson()
   }
 }
